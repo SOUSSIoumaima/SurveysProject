f2be7cd7205f922acfee53f49bf977dd8d4c4f62
SOUSSIoumaima <oumaima.soussi@horizon-tech.tn>
2025-03-13 13:12:10 +0100

    add surveyService

---
+++ surveyservice/src/test/java/com/horizon/surveyservice/SurveyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizon.surveyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
